7c640f4acad4366eb59588e5f718668d0eb9e9e8
Jaidev Attawar <jaidevattawar@Jaidevs-MacBook-Air.local>
2026-04-27 00:19:43 +0400

    removed database_module folder

--- database_module/src/main/java/Repositories/CourseDAO.java
+++
@@ -1,8 +0,0 @@
-package Repositories;
-
-import Models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDAO extends JpaRepository<Course, Long> {
-    //findby course id, cousr name, etc.
-}
